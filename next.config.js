36961a4d27732a2d79ded8f7d9ced0e1449cd525
Quang Vu <ngqangvvu@gmail.com>
2023-05-12 05:00:04 +0700

    Config domain

--- next.config.js
+++ next.config.js
@@ -4,8 +4,11 @@ const nextConfig = {
     appDir: true,
   },
   images: {
-    domains: ['res.cloudinary.com'],
-  }
-}
+    domains: [
+      "res.cloudinary.com",
+      "aws-s3-nodejs-reviewfood.s3.us-east-1.amazonaws.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
