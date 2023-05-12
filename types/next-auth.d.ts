81bb8854fff6db037565bb5aa093c7c8396964dd
Quang Vu <ngqangvvu@gmail.com>
2023-05-13 02:09:36 +0700

    Add types

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,16 +1,23 @@
 import nextAuth from "next-auth";
 declare module "next-auth" {
-    interface Session {
-        user: {
-            id: string;
-            firstName: string;
-            lastName: string;
-            email: string;
-            role: string;
-            accessToken: string;
-            data: { id: string; firstName: string; lastName: string; email: string; role: string; accessToken: string; };
-        };
-       
-    }
-
+  interface Session {
+    user: {
+      id: string;
+      firstName: string;
+      lastName: string;
+      email: string;
+      role: string;
+      accessToken: string;
+      data: {
+        id: string;
+        firstName: string;
+        lastName: string;
+        email: string;
+        role: string;
+        accessToken: string;
+        user?: any;
+        totalPost?: any;
+      };
+    };
+  }
 }
